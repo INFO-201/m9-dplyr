6bf215ff0195a80732ee2939e7d08e601af994f1
Joel Ross <joelross@uw.edu>
2017-01-26 07:49:59 -0800

    Update exercises for students

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -10,42 +10,32 @@ library(dplyr)
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # use `??flights` to search for documentation on the data set (for what cols are)
-nrow(flights)
-ncol(flights)
-colnames(flights)
-?flights
+
 
 # Use `dplyr` to give the data frame a new column that is the amount of time gained in the air
-flights <- mutate(flights, gain = arr_delay - dep_delay)
+
 
 # Use `dplyr` to sort your data.frame descending by the column you just created.
 # Remember to save this as a variable (or in the same one!)
-flights <- arrange(flights, desc(gain))
-View(head(flights))
+
 
 # For practice, repeat the last 2 steps in a single statement using the pipe operator
-flights <- flights %>% mutate(gain = arr_delay - dep_delay) %>% arrange(desc(gain))
+
 
 # Make a histogram of the amount of gain using the `hist()` function
-hist(flights$gain)
+
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-mean(flights$gain, na.rm = TRUE) # Lost 5 minutes!
+
 
 # Create a data.frame of flights headed to seatac ('SEA'), only including the
 # origin, destination, and the "gain" column you just created
-to.sea <- flights %>% select(origin, dest, gain) %>% filter(dest == 'SEA')
-to.sea
+
 
 # On average, did flights to seatac gain or loose time?
+
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement!
 # (and don't show any other data)
-filter(flights, origin=="JFK", dest=="SEA") %>%
-  summarize(
-    average.air.time = mean(air_time, na.rm = TRUE),
-    max.air.time = max(air_time, na.rm = TRUE),
-    min.air.time = min(air_time, na.rm = TRUE)
-  )
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -9,34 +9,21 @@ library(dplyr)
 # What was the average department delay in each month?
 # Save this as a data frame `dep.delay.by.month`
 # Hint: you'll have to perform a grouping operation before summarizing your data
-dep.delay.by.month <- flights %>%
-  group_by(month) %>%
-  summarize(delay = mean(dep_delay, na.rm=TRUE))
-dep.delay.by.month
+
 
 # Which month had the greatest average departure delay?
-filter(dep.delay.by.month, delay == max(delay))
+
 
 # If your above data frame contains the columns "month", and "delay", you can create
 # a scatterplot by passing that data frame to the 'plot()' function
-plot(dep.delay.by.month)
+
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-arr.delay.by.month <- flights %>%
-  group_by(dest) %>%
-  summarise(delay = mean(arr_delay, na.rm=TRUE)) %>%
-  arrange(-delay)
-arr.delay.by.month
+
 
 # You can look up these airports in the `airports` data frame!
-filter(airports, faa == arr.delay.by.month$dest[1])  # e.g.
+
 
 ### Bonus ###
 # Which city was flown to with the highest average speed?
-city.by.speed <- flights %>%
-  mutate(speed = distance/air_time * 60) %>%
-  group_by(dest) %>%
-  summarise(avg_speed = mean(speed, na.rm = TRUE)) %>%
-  arrange(-avg_speed)
-city.by.speed
